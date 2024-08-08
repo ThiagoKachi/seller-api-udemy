731213d7bb9d5797d00241627a80b1863e9640df
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-08-08 19:05:17 -0300

    feat: create product module

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,8 +2,10 @@ import 'reflect-metadata';
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { errors } from 'celebrate';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 
 const app = express();
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof AppError) {
