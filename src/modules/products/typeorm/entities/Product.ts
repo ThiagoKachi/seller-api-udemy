0b12cea3845889cd702c8f2bd9c02a10399ea8cc
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-08-13 21:07:44 -0300

    feat: create order module

--- src/modules/products/typeorm/entities/Product.ts
+++ src/modules/products/typeorm/entities/Product.ts
@@ -1,10 +1,14 @@
 /* eslint-disable indent */
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import OrdersProducts from '@modules/orders/typeorm/entities/OrderProducts';
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity('products')
 class Product {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.product)
+  order_products: OrdersProducts[];
 
   @Column('text')
   name: string;
